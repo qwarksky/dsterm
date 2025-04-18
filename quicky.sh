859e331bee7967272ede5e3826defcb7ad6eb4fa
Ezio Qwarksky <qwarks@protonmail.com>
2025-04-18 09:38:12 +0200

    pretty printf

--- quicky.sh
+++ quicky.sh
@@ -1,25 +1,25 @@
 #! /bin/sh
 echo "\n Count lines-chars-Largest line width"
-echo $(exec wc -lcL "$1")
+wc -lcL "$1"
 
 echo "\n Unique lines (exclude index)"
-echo $(cut -d',' -f2- "$1" | sort | uniq | wc -l)
+cut -d',' -f2- "$1" | sort | uniq | wc -l
 
 
 echo "\nShow first five lines:"
-echo $(exec head "$1")
+head "$1"
 
 
 echo "\n Sed : Filter lines 10 to 12"
-echo $(sed -n '10,12p' "$1" )
+sed -n '10,12p' "$1"
 
 echo "\n Sed : Filter lines 10 to 12"
-echo $(sed -n '/First/p' "$1" | head -n 2 )
+sed -n '/First/p' "$1" | head -n 3 
 
 
 echo "\n Awk :Filter lines with number line"
-echo $(awk 'NR==5, NR==12  { print NR ":" $0}' "$1" )
+awk 'NR==5, NR==12  { printf "%d : %s \n",NR,$0 }' "$1"
 
-echo "\n Awk :FFilter pattern "First" in specific field"
-echo $(awk -F','  '$10 ~ /First/ {print "Line " NR ":" $0}'  "$1" | head -n 2)
+echo "\n Awk :Filter pattern "First" in specific field"
+awk -F','  '$10 ~ /First/ {printf "Line  %d : %s \n",NR,$0}'  "$1" | head -n 3
 
